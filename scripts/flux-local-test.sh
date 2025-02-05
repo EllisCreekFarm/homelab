102200ab13daf35702ec95c095710d4568361098
Joel Baranick <jbaranick@gmail.com>
2025-02-04 17:27:02 -0800

    chore: standardize files and improve validations
    Introduce yaml formatting to standardize yaml files. Simplify the
    kubeconform and flux-local-test scripts. Include flux-local test as
    part of the pre-commit checks.

---
+++ scripts/flux-local-test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+function validate() {
+    local cluster_dir="$1"
+    shift 1
+
+    local flux_local_args=(
+        "test"
+        "${cluster_dir}/flux"
+        "$@"
+    )
+
+    echo "=== Validating ${cluster_dir}/flux ==="
+    if ! flux-local "${flux_local_args[@]}"; then
+        exit 1
+    fi
+}
+
+function run() {
+    if [[ "${DEVBOX_SHELL_ENABLED:-0}" != "1" ]]; then
+        devbox run flux-local-test "$@"
+        return $?
+    fi
+
+    validate "$DEVBOX_PROJECT_ROOT/kubernetes"
+}
+
+run "$@"
